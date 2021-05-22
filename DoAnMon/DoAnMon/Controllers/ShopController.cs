3582101d1d66cbf91cd6ce3b2abaac4e1f5638e7
Spader <66111744+hieu555x@users.noreply.github.com>
2021-05-22 13:36:03 +0700

    22/05/21 1:35

---
+++ DoAnMon/DoAnMon/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using DoAnMon.Models;
+
+namespace DoAnMon.Controllers
+{
+    public class ShopController : Controller
+    {
+        dbBanHangOnlineEntities data = new dbBanHangOnlineEntities();
+        // GET: Shop
+        public ActionResult Index(int ? page)
+        {
+            int pageSize = 8;
+            int pagenum = (page ?? 1);
+            return View(data.sanPhams.OrderByDescending(a=>a.ngayDang).ToList().ToPagedList(pagenum,pageSize));
+        }
+
+        public ActionResult ChiTiet(string id)
+        {
+            return View(data.sanPhams.SingleOrDefault(a => a.maSP == id));
+        }
+    }
+}
